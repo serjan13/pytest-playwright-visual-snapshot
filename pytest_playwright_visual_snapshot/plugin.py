6905badcac2639b05818aeaf8f088b9d114d5dcb
Pinchuk Sergei <s.pinchuk@spinchuk.local>
2026-09-21 16:45:37 +0300

    add postresql logger propogate False

--- pytest_playwright_visual_snapshot/plugin.py
+++ pytest_playwright_visual_snapshot/plugin.py
@@ -21,6 +21,7 @@ stream_handler = logging.StreamHandler()
 stream_handler.setLevel(log_level)
 logging.basicConfig(level=log_level, handlers=[stream_handler])
 logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("PostgreSQL").propagate = False
 
 logger = logging.getLogger(__name__)
 
